1db96479a72ad02a67725e55d0c6547994c8de80
JimmyQYHuang <jimmyqyhuang@ami.com>
2024-12-25 17:52:46 +0800

    add

---
+++ CMakeFiles/app.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/app.dir/main.cpp.o"
+  "CMakeFiles/app.dir/main.cpp.o.d"
+  "CMakeFiles/app.dir/tests/custom_test.cpp.o"
+  "CMakeFiles/app.dir/tests/custom_test.cpp.o.d"
+  "app"
+  "app.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/qy/project/test
+# Build directory: /home/qy/project/test/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
